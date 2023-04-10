a134a909425164ebc20f32580556bc072c977a39
enovak89 <enovak89@mail.ru>
2023-04-10 23:38:05 +0300

    point 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import java.lang.reflect.Array;
+import java.sql.Array;
 import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) {
-        StringListImpl array = new StringListImpl(new String[4], 5);
-        System.out.println(array.add("AAA"));
+        StringListImpl array = new StringListImpl(new String[3], 3);
+        System.out.println(array.add("A"));
+        System.out.println(array.add("B"));
+        System.out.println(array.add("C"));
+        System.out.println(array.getStringArray()[0]);
+        System.out.println(array.getStringArray()[1]);
+        System.out.println(array.getStringArray()[2]);
 
     }
 }
--- src/StringListImpl.java
+++ src/StringListImpl.java
@@ -2,11 +2,15 @@ import exceptions.IncorrectArgumentException;
 import exceptions.MaxArrayLengthAchievedException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class StringListImpl {
 
     private final String[] stringArray;
     private final int arrayMaxLength;
+    private int count = 0;
+
+
 
     public StringListImpl(String[] stringArray, int arrayMaxLength) {
         this.stringArray = stringArray;
@@ -20,7 +24,7 @@ public class StringListImpl {
     }
 
     private void checkArray() {
-        if (stringArray.length >= arrayMaxLength) {
+        if (stringArray.length > arrayMaxLength) {
             throw new MaxArrayLengthAchievedException("Достигнут максимальный размер массива: " + arrayMaxLength);
         }
     }
@@ -28,7 +32,11 @@ public class StringListImpl {
     public String add(String item) {
       checkArgument(item);
       checkArray();
-      return stringArray[stringArray.length - 1] = item;
+      return stringArray[count++] = item;
+    }
+
+    public String[] getStringArray() {
+        return stringArray;
     }
 
 }
